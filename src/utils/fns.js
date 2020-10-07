a28ad5376e09e3adb8e824c04a3c8c1ed2866957
Antonio Oropeza <jaop.7@outlook.com>
2020-10-07 10:59:13 -0500

    - Remove unused function

--- src/utils/fns.js
+++ src/utils/fns.js
@@ -1,9 +1,5 @@
-function random(limit) {
-  return Math.floor(Math.random() * limit);
-}
-
 function truncateStr(string) {
   return string?.substring(0, 500);
 }
 
-export { random, truncateStr };
+export { truncateStr };
